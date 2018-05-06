3bf3cf5ad13fcf95e7359d93f65c6be754cd3eac
Fatia <fatia.dewi@gmail.com>
2018-05-07 00:10:44 +0700

    add statistic page on metric controller

--- app/controllers/metric_controller.rb
+++ app/controllers/metric_controller.rb
@@ -1,2 +1,44 @@
 class MetricController < ApplicationController
+
+  def statistic
+    @metric = Metric.where(redash_id: params[:id]).first
+
+    if params[:start]
+      start_date = Time.zone.parse(params[:start]).beginning_of_day
+    else
+      start_date = Time.zone.now.beginning_of_month
+    end
+    if params[:end]
+      end_date = Time.zone.parse(params[:end]).end_of_day
+    else
+      end_date = Time.zone.now.end_of_month
+    end
+
+    alerts = @metric.alerts.where('alerts.created_at >= ? AND alerts.created_at < ?', start_date, end_date)
+
+
+    total_alert = alerts.count
+    total_upper_alert = alerts.where(is_upper: true).count
+    total_lower_alert = alerts.where(is_upper: false).count
+    maximum_value = alerts.where(is_upper: true).maximum(:value)
+    average_upper_value = alerts.where(is_upper: true).average(:value)
+    minimum_value = alerts.where(is_upper: false).maximum(:value)
+    average_lower_value = alerts.where(is_upper: false).average(:value)
+    graph_data = Statistic.calculate_alert_graph_data(alerts)
+
+    @result = {
+      total_alert: total_alert,
+      total_upper_alert: total_upper_alert,
+      total_lower_alert: total_lower_alert,
+      maximum_value: maximum_value,
+      average_upper_value: average_upper_value,
+      minimum_value: minimum_value,
+      average_lower_value: average_lower_value,
+      graph_data: graph_data
+    }.to_json
+  end
+
+  def manage
+    @metrics = Metric.all.paginate(:page => params[:page], :per_page => 10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :dashboard, only: [] do
-  	collection do
-  		get :summary
-  	end
+    collection do
+      get :summary
+    end
   end
 
+  resources :metric, only: [] do
+    member do
+      get :statistic
+    end
+  end
 end
