b4939b729fb4dd5cdbeb70e70e87484b9711c95d
Fatia <fatia.dewi@gmail.com>
2018-05-07 22:12:08 +0700

    add method on metric model for update, set, and send alert

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -4,10 +4,16 @@ class Metric < ApplicationRecord
 
 
 	def update_threshold
-		threshold = HawkPython.get_threshold(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_unit)
-		self.upper_threshold = threshold[:upper_threshold]
-		self.lower_threshold = threshold[:lower_threshold]
+		threshold = HawkPython.update_threshold(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_type)
+		self.upper_threshold = threshold['upper_threshold']
+		self.lower_threshold = threshold['lower_threshold']
 	end
+
+  def set_threshold
+    threshold = HawkPython.set_threshold(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_type)
+    self.upper_threshold = threshold['upper_threshold']
+    self.lower_threshold = threshold['lower_threshold']
+  end
 
 	def send_alert
 		HawkPython.send_alert_hawk(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_unit, self.upper_threshold, self.lower_threshold)
