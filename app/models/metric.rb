71714edcfcf649d7ceddcb4973d6aaf6eba6f436
Fatia <fatia.dewi@gmail.com>
2018-05-08 14:02:29 +0700

    fix param

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -4,19 +4,19 @@ class Metric < ApplicationRecord
 
 
 	def update_threshold
-		threshold = HawkPython.update_threshold(self.redash_id, self.time_column, self.value_column, self.value_type, self.time_unit)
+		threshold = HawkPython.update_threshold(self.redash_id, self.time_column, self.value_column, self.value_type)
 		self.upper_threshold = threshold['upper_threshold']
 		self.lower_threshold = threshold['lower_threshold']
 	end
 
   def set_threshold
-    threshold = HawkPython.set_threshold(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_type)
+    threshold = HawkPython.set_threshold(self.redash_id, self.time_column, self.value_column, self.value_type)
     self.upper_threshold = threshold['upper_threshold']
     self.lower_threshold = threshold['lower_threshold']
   end
 
 	def send_alert
-		HawkPython.send_alert_hawk(self.redash_id, self.time_column, self.value_column, self.time_unit, self.value_type, self.upper_threshold, self.lower_threshold)
+    HawkPython.send_alert_hawk(self.redash_id, self.time_column, self.value_column, self.value_type, self.time_unit,  self.upper_threshold, self.lower_threshold, Time.zone.now.strftime('%F %H:%M:%S'), self.email)
 	end
 
 	def to_hash
